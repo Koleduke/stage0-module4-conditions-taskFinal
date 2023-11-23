d16638bf31482056602225cfa5be4f4e909e6f06
Koleduke <ydel2014@gmail.com>
2023-11-23 15:44:32 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        if(monthNumber<1||monthNumber>12){
+            System.out.println("Wrong month number");
+        }
+        else{
+            if(monthNumber==12||monthNumber<3)
+            {System.out.println("Winter");}
+            if(monthNumber>2&&monthNumber<6)
+            {System.out.println("Spring");}
+            if(monthNumber>5&&monthNumber<9)
+            {System.out.println("Summer");}
+            if(monthNumber>8&&monthNumber<12)
+            {System.out.println("Autumn");}
+
+
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,31 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        double l, m, s;
+        if (firstSide<=0||secondSide<=0||thirdSide<=0){
+            System.out.println("it's not a triangle");
+        }   else {
 
-    }
+            if(firstSide>=secondSide&&firstSide>=thirdSide){l=firstSide;}
+            else{
+                if(secondSide>=firstSide&&secondSide>=thirdSide){l=secondSide;}
+                else{l=thirdSide;}
+            }
+
+        if((firstSide>=secondSide&&firstSide<=thirdSide)||(firstSide<=secondSide&&firstSide>=thirdSide)){m=firstSide;}
+        else{
+            if((secondSide>=firstSide&&secondSide<=thirdSide)||(secondSide<=firstSide&&secondSide>=thirdSide)){m=secondSide;}
+            else{m=thirdSide;}
+        }
+
+        if(firstSide<=secondSide&&firstSide<=thirdSide){s=firstSide;}
+        else{
+            if(secondSide<=firstSide&&secondSide<=thirdSide){s=secondSide;}
+            else{s=thirdSide;}
+        }
+
+        if ((s+m)<l){
+            System.out.println("it's not a triangle");
+        } else{System.out.println("this is a valid triangle");}
+    }}
 }
